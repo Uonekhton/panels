f525478979133eec909732ce373d213f6f5ec8ec
Ilya Khramtsov <minigun@protonmail.com>
2017-02-22 00:59:52 +0500

    Добавил шаблон

--- main.py
+++ main.py
@@ -1,22 +1,43 @@
-import asyncio
-import subprocess
+import asyncio, jinja2, aiohttp_jinja2, await, subprocess
 from aiohttp import web
+
+
+commands = {
+    0: 'запущен',
+    3: 'остановлен',
+    127: 'недостаточно прав',
+}
+
+service = (
+    'nginx',
+    'mysql',
+)
 
 
 @asyncio.coroutine
 def handle(request):
-    status = subprocess.call('service nginx status',
-                    shell=True,
-                    stdout=open('/dev/null', 'w'),
-                    stderr=subprocess.STDOUT
-                    )
-    if status == 3:
-        text = 'Сервис Nginx остановлен'
-    elif status == 0:
-        text = 'Сервис Nginx работает'
-    return web.Response(text=text)
+    context = {}
+    for daemon in service:
+        status = subprocess.call('service %s status' %daemon,
+                        shell=True,
+                        stdout=open('/dev/null', 'w'),
+                        stderr=subprocess.STDOUT
+                        )
+        tt = {daemon: commands[status]}
+        context.update({daemon: tt})
+
+    return aiohttp_jinja2.render_template('index.html', request, context)
+
+
+@asyncio.coroutine
+def command(self):
+    data = await.processes(self.request.post())
+    pass
 
 app = web.Application()
+aiohttp_jinja2.setup(app,
+    loader=jinja2.FileSystemLoader('templates/'))
+app.router.add_static('/static/', path='static/', name='static')
 app.router.add_get('/', handle)
 
 web.run_app(app)
