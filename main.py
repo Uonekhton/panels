320b0688b0cb7ebd5079a09867ad25fb46dc058d
Ilya Khramtsov <minigun@protonmail.com>
2017-02-27 13:59:05 +0500

    сервис работает

--- main.py
+++ main.py
@@ -2,42 +2,56 @@ import asyncio, jinja2, aiohttp_jinja2, await, subprocess
 from aiohttp import web
 
 
-commands = {
+signal2 = {
     0: 'запущен',
     3: 'остановлен',
     127: 'недостаточно прав',
 }
 
+
 service = (
     'nginx',
     'mysql',
 )
-
+        
 
 @asyncio.coroutine
 def handle(request):
-    context = {}
-    for daemon in service:
-        status = subprocess.call('service %s status' %daemon,
-                        shell=True,
-                        stdout=open('/dev/null', 'w'),
-                        stderr=subprocess.STDOUT
-                        )
-        tt = {daemon: commands[status]}
-        context.update({daemon: tt})
+    daemons = {}
+    signal = signal2
+    for item in service:
+        status = Command._status(item)
+        daemon = item
+        daemons[daemon] = status
 
-    return aiohttp_jinja2.render_template('index.html', request, context)
+    return aiohttp_jinja2.render_template('index.html', request, {'daemons': daemons,
+                                                                  'signal': signal,
+                                                                  })
 
 
 @asyncio.coroutine
-def command(self):
-    data = await.processes(self.request.post())
-    pass
+class Command:
+    def _status(item):
+        status = subprocess.call('service %s status' % item,
+                                 shell=True,
+                                 stdout=open('/dev/null', 'w'),
+                                 stderr=subprocess.STDOUT
+                                 )
+        return status
+    
+    def panel(request):
+        panel = subprocess.call('service %s %s' % (request.url.query.get('daemon'), request.url.path.replace('/', '')),
+                                shell=True,
+                                stdout=open('/dev/null', 'w'),
+                                stderr=subprocess.STDOUT
+                                )
+        return web.HTTPFound('/')
 
 app = web.Application()
 aiohttp_jinja2.setup(app,
     loader=jinja2.FileSystemLoader('templates/'))
 app.router.add_static('/static/', path='static/', name='static')
 app.router.add_get('/', handle)
+app.router.add_get('/{command}', Command.panel)
 
 web.run_app(app)
